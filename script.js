1af22b48e61ed97c04254e4be06f1551426cd4d9
Sarthak Gupta <sarthakuniverse007@gmail.com>
2026-02-20 10:35:57 +0530

    .\script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// console.log("Hello World");
+// document.write("Hello World");
+
+
+var a=100;var b=13; var c=1; var linebreak="<br";
+
+document.write("a+b+c=");
+result=a+b+c;
+document.write(" "+result);
+document.write(linebreak);
