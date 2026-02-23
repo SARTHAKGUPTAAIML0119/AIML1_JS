cf57de1f63bc335e356fb5b42b6f2f238369a6c5
Sarthak Gupta <sarthakuniverse007@gmail.com>
2026-02-23 15:39:37 +0530

    OPERATORS AND LOOPS

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 // console.log("Hello World");
 // document.write("Hello World");
 
+// var a=10; var b=20;
+// var linebreak="<br/>";
 
-var a=100;var b=13; var c=1; var linebreak="<br";
+// document.write("(a==b)=>");
 
-document.write("a+b+c=");
-result=a+b+c;
-document.write(" "+result);
-document.write(linebreak);
+// result =(a<b);
+
+// document.write(result);
+// document.write(linebreak);
+
+// result=(a>b);
+// document.write(result);
+
+// document.write(linebreak);
+
+// for(var i=0;i<9;i++){
+// 	console.log(i);
+//     document.write(i);
+//     document.write(linebreak);
+// }
+
+
+const person={
+    fname: "Ajay", lname:"singh",age:25
+};
+
+for(let x in person){
+    console.log("person details: "+x+": "+person[x]);
+}
