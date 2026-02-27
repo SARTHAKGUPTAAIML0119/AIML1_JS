71cd402341587e8e703ff71aa68042ed1cd65f88
Sarthak Gupta <sarthakuniverse007@gmail.com>
2026-02-27 10:28:54 +0530

    Lecture 5

--- script.js
+++ script.js
@@ -23,10 +23,95 @@
 // }
 
 
-const person={
-    fname: "Ajay", lname:"singh",age:25
-};
+// const person={
+//     fname: "Ajay", lname:"singh",age:25
+// };
 
-for(let x in person){
-    console.log("person details: "+x+": "+person[x]);
-}
+// for(let x in person){
+//     console.log("person details: "+x+": "+person[x]);
+// }
+
+// const person={
+//     fname:"Ajay",
+//     lname:"singh",
+//     age:25
+// };
+// for(let x in person){
+//     console.log("Person details:"+x+":"+person.x)
+// }
+
+// var count=0;
+// while(count<10){
+//     document.write(count);
+//     count++;
+// }
+
+// var age=20;
+// if(age>18){
+//     document.write("<b>You are eligible for voting.</b>");
+// }
+// else{
+//     document.write("<b>You are not eligible for voting.</b>");
+// }
+
+// var grade="a";
+// switch(grade){
+//     case "a":document.write("Good Job<br/>");
+//     break;
+//     case "b":document.write("pretty good<br/>");
+//     break;
+//     case "c":document.write("Pass<br/>");
+//     break;
+//     case "d":document.write("fail<br/>");
+//     break;
+//     default:document.write("Unknown grade<br/>")
+// }
+
+// function myfunc(){
+//     alert("HELLO WORLD!")
+// }
+
+// function sayHello(name,age){
+//     document.write(name+" is "+age+" years old.")
+// }
+
+// var res=mul(12,34);
+// function mul(x,y){
+//     return x*y;
+// }
+// document.write(res);
+
+// function add(a,b){
+//     console.log(a+b);
+// }add(10,20);
+
+// var add2=(a,b)=>{
+//     console.log(a+b);
+// }
+// add2(20,30);
+
+// var add3=(a,b)=>console.log(a+b);
+
+// add3(100,101);
+
+// var person={
+//     name:"john mac",
+//     age:20,
+//     isMarried:true,
+//     adress:{
+//         street:'vinay nagar',
+//         flat:201
+//     }
+// };
+
+// console.log(person);
+// document.write(person["name"])
+
+var person2= new Object();
+person2.name="bombastic";
+person2.age=24;
+person2.adress={};
+person2.adress.street="nehru";
+person2['adress']['flatno']=201;
+console.log(person2);
+
